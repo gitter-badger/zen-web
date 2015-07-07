5a2162a0d1c4fa06dd35d3caf8fffc012d5704cd
bryan-huangyan <bryan@idevops.net>
2015-07-07 13:23:02 +0800

    update misc.py

---
+++ zensite/widgets/misc.py
@@ -0,0 +1,37 @@
+#
+# Licensed Materials - Property of esse.io
+#
+# (C) Copyright esse.io. 2015 All Rights Reserved
+#
+# Author: Bryan HUANG (bryan@esse.io)
+#
+#
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# from django.forms.widgets import flatatt
+
+
+def render_logo(href, mini_logo_highlight, mini_logo, lg_logo_highlight, lg_logo):
+	logo_html = """
+	    <!-- Logo -->
+        <a href='{href}' class='logo'>
+          <!-- mini logo for sidebar mini 50x50 pixels -->
+          <span class='logo-mini'><b>{mini_logo_highlight}</b>{mini_logo}</span>
+          <!-- logo for regular state and mobile devices -->
+          <span class='logo-lg'><b>{lg_logo_highlight}</b>&nbsp;{lg_logo}</span>
+        </a>
+        """
+
+	return logo_html.format(
+    	href=href,
+    	mini_logo_highlight=mini_logo_highlight,
+    	mini_logo=mini_logo,
+    	lg_logo_highlight=lg_logo_highlight,
+    	lg_logo=lg_logo
+    	)
+
+if __name__ == "__main__":
+    html = render_logo("http://esse.io", "Z", "en", "Zen", "esse.io")
+    print html
