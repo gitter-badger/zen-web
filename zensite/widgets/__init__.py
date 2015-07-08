b242f144e75fe041039e17fe8a8fbf9cc31305f8
bryan-huangyan <bryan@idevops.net>
2015-07-08 22:18:07 +0800

    navbar

--- zensite/widgets/__init__.py
+++ zensite/widgets/__init__.py
@@ -21,7 +21,7 @@ class ZenWidget(Widget):
     text = ""
     name = ""
 
-    def __init__(self, element_name, name=None, id=None, attrs=None):
+    def __init__(self, element_name, attrs=None, id=None, name=None):
         if id:
             self.id = id
         if attrs:
@@ -32,23 +32,41 @@ class ZenWidget(Widget):
         if name:
             self.name = name
 
-    def append_child(self, text):
+    def append_text(self, text):
         self.text = text
 
     def append_widget(self, widget):
         self.sub_widgets.append(widget)
 
     def render(self):
-        final_attrs = self.build_attrs(self.attrs, name=self.name, id=self.id)
+        if self.name:
+            self.attrs['name'] = self.name
+        if self.id:
+            self.attrs['id'] = self.id
+        final_attrs = self.build_attrs(self.attrs)
         html = "<{}".format(self.element_name)
         html = html + "{}"
         if self.text:
             html = html + " >{}".format(self.text) + "</{}>".format(self.element_name)
+        elif self.sub_widgets:
+            extra_appending = self.extra_appending()
+            if extra_appending:
+                html += extra_appending.render()
+            for widget in self.sub_widgets:
+                html += widget.render()
+            if extra_appending:
+                html += extra_appending.end_html()
+            html = html + "</{}>".format(self.element_name)
         else:
             html = html + " />"
         return format_html(html, flatatt(self.attrs))
 
+    def end_html():
+        return ""
+`
+    def extra_appenging(self):
+        return None
 
 if __name__ == "__main__":
-    zw = ZenWidget("div", "", "test", {"class": "test", "width": "100%"})
+    zw = ZenWidget("div", {"class": "test", "width": "100%"}, "testid", "test")
     print zw.render()
--- zensite/widgets/nav.py
+++ zensite/widgets/nav.py
@@ -13,5 +13,39 @@ from __future__ import unicode_literals
 # from django.forms.widgets import flatatt
 
 
-class Nav:
-	
+class NavUl(ZenWidget):
+    def __init__(self, css_classes):
+        super("ul", {"class": css_classes})
+        if not css_classes:
+            self.css_classes = "nav navbar-nav"
+            self.attrs["class"] = css_classes
+        else:
+            self.css_classes = css_classes
+
+    def end_html(self):
+        return "</ul>"
+
+class NavDiv(ZenWidget):
+    def __init__(self, css_classes):
+        super("div", {"class": css_classes})
+        if not css_classes:
+            self.css_classes = "navbar-custom-menu"
+            self.attrs["class"] = css_classes
+        else:
+            self.css_classes = css_classes
+
+    def extra_appending(self):
+        return NavUl("nav navbar-nav")
+
+    def end_html(self):
+        return "</div>"
+
+class Nav(ZenWidget):
+    def __init__(self, css_classes, id=None):
+        super("nav", {"class": css_classes}, id)
+        if not css_classes:
+            self.css_classes = "navbar navbar-static-top"
+            self.attrs["class"] = css_classes
+
+    def extra_appending():
+        return NavDiv("navbar-custom-menu")
