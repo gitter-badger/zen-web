4bbda3ff5c12efa59e161cf3fa6c99509567e80d
bryan-huangyan <bryan@idevops.net>
2015-07-09 11:59:20 +0800

    navbar update

--- zensite/widgets/nav.py
+++ zensite/widgets/nav.py
@@ -11,11 +11,12 @@
 from __future__ import unicode_literals
 
 # from django.forms.widgets import flatatt
+from base import ZenWidget
 
 
 class NavUl(ZenWidget):
     def __init__(self, css_classes):
-        super("ul", {"class": css_classes})
+        super(self.__class__, self).__init__("ul", {"class": css_classes})
         if not css_classes:
             self.css_classes = "nav navbar-nav"
             self.attrs["class"] = css_classes
@@ -25,9 +26,10 @@ class NavUl(ZenWidget):
     def end_html(self):
         return "</ul>"
 
+
 class NavDiv(ZenWidget):
     def __init__(self, css_classes):
-        super("div", {"class": css_classes})
+        super(self.__class__, self).__init__("div", {"class": css_classes})
         if not css_classes:
             self.css_classes = "navbar-custom-menu"
             self.attrs["class"] = css_classes
@@ -40,12 +42,21 @@ class NavDiv(ZenWidget):
     def end_html(self):
         return "</div>"
 
+
 class Nav(ZenWidget):
-    def __init__(self, css_classes, id=None):
-        super("nav", {"class": css_classes}, id)
+    def __init__(self,
+                 css_classes="navbar navbar-static-top",
+                 role="navigation",
+                 id=None):
+        super(self.__class__, self).__init__("nav", {"class": css_classes, "role": role}, id)
         if not css_classes:
             self.css_classes = "navbar navbar-static-top"
             self.attrs["class"] = css_classes
 
-    def extra_appending():
+    def extra_appending(self):
         return NavDiv("navbar-custom-menu")
+
+
+if __name__ == "__main__":
+    nav = Nav()
+    print(nav.render())
